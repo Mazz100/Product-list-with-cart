24af2307f235541eb62102f2231206e0d2a4bf4f
Mazen Hassan <95134348+Mazz100@users.noreply.github.com>
2024-07-17 20:12:16 +0300

    Initial commit
    - Vite template for react
    
    - Project files
    
    - Tailwind setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <main></main>
+      <footer></footer>
+    </>
+  );
+}
+
+export default App;
