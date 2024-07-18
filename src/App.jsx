41da856f9deefe68a94eb7b4fb92041f200df121
Mazen Hassan <95134348+Mazz100@users.noreply.github.com>
2024-07-18 19:44:44 +0300

    Mobile design layout
    - Mobile design complete
    
    - Installed prettier with tailwind plug-in for sorting class order

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 import { useState } from "react";
 import "./App.css";
+import ProductList from "./ProductList";
+import ProductCart from "./ProductCart";
 
 function App() {
   return (
     <>
-      <main></main>
-      <footer></footer>
+      <div className="min-h-screen font-red-hat-text p-4 flex flex-col items-center bg-body-bg-color">
+        <main className="max-w-[24rem]">
+          <h1 className="text-4xl font-bold">Desserts</h1>
+          <ProductList />
+          <ProductCart />
+        </main>
+        <footer></footer>
+      </div>
     </>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,26 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        "red-hat-text": ["Red Hat Text", "sans-serif"],
+      },
+
+      backgroundColor: {
+        "body-bg-color": "#fcf9f7",
+      },
+
+      colors: {
+        "primary-color": "#c73a0f",
+        "secondary-color": "#1ea475",
+      },
+
+      borderColor: {
+        "border-color-light": "#ad8985",
+        "border-color-medium": "#87635a",
+        "border-color-strong": "#260f08",
+      },
+    },
   },
   plugins: [],
 };
